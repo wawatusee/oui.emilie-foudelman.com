c731cd875e9dd7af685b74da11ddd3d355f741de
wawatusee <web2020.kieran.labarrere@gmail.com>
2024-09-30 01:44:50 +0200

    readme, complete comments

--- inc/head.php
+++ inc/head.php
@@ -3,13 +3,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="keywords" content="">
-    <meta property="og:title" content="SlowwaysWeekend, programme">
-    <meta property="og:type" content="article" />
-    <meta property="og:url" content="https://slowwaysweekend.be/public/index.php#programm">
-    <meta property="og:image" content="https://slowwaysweekend.be/public/img/content/swwfacebook-withdates.jpg">
-    <meta property="og:image:width" content="989">
-    <meta property="og:image:height" content="720">
-    <meta property="og:site_name" content="slowwaysweekend.be">
+<!--ici les balises og de FaceBook pour option de partage de la page-->
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/header.css">
     <link rel="stylesheet" href="css/menu.css">
@@ -18,6 +12,4 @@
     <script src="js/menu.js"></script>
     <link rel="shortcut icon" type="image/png" href="favicon.ico">
     <title><?= $titleWebSite[0].$titleWebSite[1].$titleWebSite[2].'.be'?></title>
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
-    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
 </head>
