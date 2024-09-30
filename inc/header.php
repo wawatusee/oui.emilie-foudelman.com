f1a5a502896f2228921903099f460f0fc2b48939
wawatusee <web2020.kieran.labarrere@gmail.com>
2024-09-30 15:17:39 +0200

    header.css, menu.css, style.css, Couleurs de header, disposition, modif structure

--- inc/header.php
+++ inc/header.php
@@ -1,25 +1,12 @@
 <header>
     <div class="mainTitleBlock">
-        <h1><a href="https://www.emilie-foudelman.com/" target=”_blank”><img class="logo" src='<?=$repMedias.'/deco/logo_blanc.svg'?>' alt="lien vers le site emilie-foudelman.com"></a></h1>
-        <div class="mainsubtitle"><?php echo $titleWebSite[0]?><?php echo " ".$titleWebSite[1]?><?php echo " ".$titleWebSite[2]?>
-        <div class="menulangues">
-        <?php //Liste déroulante des langues
-        echo '<form method="get">';
-       // echo '<select name="lang" id="lang" onchange="this.form.submit()">';
-        foreach ($langues_disponibles as $code_langue => $nom_langue) {
-            echo '<option value="' . $code_langue . '"';
-            if ($lang === $code_langue) {
-                echo ' selected';
-            }
-            echo '>' . $code_langue . '</option>';
-        }
-        echo '</select>';
-        echo '</form>';
-        //Fin liste déroulante des langues?>
+        <a href="https://www.emilie-foudelman.com/" target=”_blank”><img class="logo" src='<?=$repMedias.'/deco/logo_blanc.svg'?>' alt="lien vers le site emilie-foudelman.com"></a>
+
+        <div class="mainsubtitle">
+            <h1><?php echo $titleWebSite[0]?><?php echo " ".$titleWebSite[1]?><?php echo " ".$titleWebSite[2]?></h1>
         </div>
     </div>
-    </div>
     <div class="menu">
-        <?php require_once "../inc/nav.php"?>
+            <?php require_once "../inc/nav.php"?>
     </div>
 </header>
