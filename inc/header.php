5d670b8b32af75f5575e23fe7df053e3805435e0
wawatusee <web2020.kieran.labarrere@gmail.com>
2024-10-09 18:04:36 +0200

    header.php, menu.css, header.css, esthétique du titre et des éléments de menu principal calquées sur titre de page d'accueil

--- inc/header.php
+++ inc/header.php
@@ -3,9 +3,9 @@
        <!-- <a href="https://www.emilie-foudelman.com/" target=”_blank”><img class="logo" src='<?=$repMedias.'/deco/logo_blanc.svg'?>' alt="lien vers le site emilie-foudelman.com"></a>-->
        
         <div class="mainsubtitle">
-            <h1><?php echo $titleWebSite[0]?><?php echo " ".$titleWebSite[1]?><?php echo " ".$titleWebSite[2]?></h1>
+           <h1><a href="https://www.emilie-foudelman.com/"  target=”_blank”>Emilie Foudelman</a>, mariage</h1>
         </div>
-        <a href="https://www.emilie-foudelman.com/" class="btn-main" target=”_blank”>e-f</a>
+       <!-- <a href="https://www.emilie-foudelman.com/" class="btn-main" target=”_blank”>Emilie Foudelman</a>-->
     </div>
     <div class="menu">
             <?php require_once "../inc/nav.php"?>
--- inc/pages/contact.php
+++ inc/pages/contact.php
@@ -1,1 +1,11 @@
 <h2>contacts</h2>
+<div class="contacts-container">
+    <ul class="contacts">
+        <li> <a href="info@emilie-foudelman.com">info@emilie-foudelman.com</a>
+        </li>
+        <li> <a href="tel:+41762416369">Tel : +41(0)762 41 63 69</a>
+        </li>
+        <li> <a aria-label="Chat on WhatsApp" href="https://wa.me/+32486100573"><span><img alt="Chat on WhatsApp" src="../public/img/deco//whatsappbtnwithtext.svg"></span></a>
+        </li>
+    </ul>
+</div>
