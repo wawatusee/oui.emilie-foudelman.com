4d52876cd921ecfc540dc4cb6a343cf4f3bdc513
wawatusee <web2020.kieran.labarrere@gmail.com>
2024-10-10 10:33:27 +0200

    model_gallerry, création de la classe et instanciation réussie

--- inc/pages/wedding-pictures.php
+++ inc/pages/wedding-pictures.php
@@ -1,1 +1,13 @@
 <h2>photos de mariage</h2>
+
+<?php 
+require_once("../src/model/gallery_model.php");
+$cheminImages=$repImg."galleries/c-p/original";
+try {
+    $gallery = new Model_gallery($cheminImages, 'image/jpeg'); // On peut préciser 'image/png' par exemple
+    $images = $gallery->getImages();
+    print_r($images); // Affiche les informations des images trouvées
+} catch (Exception $e) {
+    echo "Erreur : " . $e->getMessage();
+}
+?>
