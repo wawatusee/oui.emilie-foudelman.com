191c003f1aeec9ecea95fb16a0bae79bd50d5aff
wawatusee <web2020.kieran.labarrere@gmail.com>
2024-10-11 12:40:52 +0200

    texte

--- inc/pages/prestation.php
+++ inc/pages/prestation.php
@@ -1,25 +1,31 @@
 <article>
   <p>
-    Photographe professionnel de mariage, Diplômée avec plus de vingt ans d’expérience. <br>
-    On dit de mes photos qu’elles sont sensibles, justes et d’une grande créativité. Et de moi que j’aime mes sujets et que cet amour transparaît dans mes clichés.
-    Photographe tout terrain, à la fois discrète et présente partout, d’une grande écoute, je m’adapte facilement à vos attentes.<br>
-    Chacun est unique c’est pourquoi je propose un service complètement personnalisé.
-    Du reportage complet ou à la carte. De trois heures à trois jours.
-    Union civile ou/et religieuse, élopement, style traditionnel, champêtre, rockabilly, fleuri ou autre.
-    <br>
-    Des préparatifs à la Lambada et si vous le désirez, même jusqu'à la gueule de bois.
-    Passant par la coiffure, le maquillage, l’habillage, l’accueil des invités, fleurs, la commune, l’église, vin d’honneur, plans de table, décoration, réception, repas, pièce montée, lancé du bouquet, jarretière, les pépites improvisées, surprises des témoins, ouverture de bal, dance-floor, et même votre tête quand le dj lance la dance des canards…
-    <br>
-    Chère mariée, je serai votre miroir, là fidèle gardienne de votre paquet de mouchoirs, ou de fard à paupières, prête à dégainer l'eye-liner.
-    J’ai des yeux partout/sens aigu de l’observation.
-    <br>
-    Je serai aussi votre ombre, celle qui va immortaliser l'instant où Damien, votre petit cousin rencontrera pour la première fois la petite Justine, sa future concubine dans quelques années.
-    Tous nous en rigolerons quand les images défileront lors de leur union. <br>
-    Croyez moi, si j'ai confiance (aveugle) en moi pour vos photos de mariage, vous pouvez aussi.
-    Si avec ses mots rigolos j'essaye de vous appâter, du sérieux de mon boulot je vous épaterez. <br>
-    Mon moment préféré, si vous me le permettez, ce sera vos larmes de joie quand je ferai vous défiler tous les clichés, mémorables et authentiques.
-    Mon travail est une sorte d’inventaire mémoriel ,de rendre l’éphémère éternel.. Histoire -mémoire. <br>
-    Pour voir si ça colle entre nous je peux vous proposer un premier rendez-vous ou même soyons fous, une séance photo de couple, ou mise en scène (ou non) de votre demande, de vos fiançailles.<br>
-    Belge installée en Suisse, je reste mobile et ouverte à toute proposition.
+  On dit de mes photos qu’elles sont sensibles, justes et d’une grande créativité. Et de moi que j’aime mes sujets et que cet amour transparaît dans mes clichés.
+Photographe tout terrain, à la fois discrète et présente partout, d’une grande écoute, je m’adapte facilement à vos attentes. <br><br>
+
+Chacun est unique c’est pourquoi je propose un service complètement personnalisé.<br>
+Du reportage complet ou à la carte. De trois heures à trois jours. <br>
+Union civile ou/et religieuse, élopement, style traditionnel, champêtre, rockabilly, fleuri ou autre.<br><br>
+
+Des préparatifs à la Macarena et si vous le désirez, même jusqu'à la gueule de bois. <br>
+En passant par la coiffure, le maquillage, l’habillage, l’accueil des invités, les fleurs, la commune, l’église, le vin d’honneur, les plans de table, la décoration, réception, repas, pièce montée, lancé du bouquet, jarretière, les pépites improvisées, surprises des témoins, ouverture de bal, dance-floor, et plus encore...<br><br>
+
+Chère mariée, je serai votre miroir, la fidèle gardienne de votre paquet de mouchoirs, votre bras droit, prête à dégainer le mascara. <br>
+Tel un espion, j'ai un sens aigu de l’observation et en toute discrétion.<br>
+Les yeux partout. Je serai aussi votre ombre, celle qui va immortaliser l'instant où Damien, votre petit cousin rencontrera pour la première fois la petite Justine, sa future concubine. <br>
+Mais aussi le slow de grand maman avec avec votre beau frère Bertrand. <br>
+Ou Milou endormi sur les genoux de tonton Henri, lui qui a toujours dit que les toutous n’étaient pas ses amis. <br><br>
+
+Croyez moi, si j'ai confiance (aveugle) en moi pour vos photos de mariage, vous pouvez aussi.  <br>
+Si avec ses mots rigolos j'essaye de vous charmer/captiver (?) du sérieux de mon boulot je vous épaterez. <br>
+Mon moment préféré, si vous me le permettez, ce sera vos larmes de joie quand je ferai vous défiler tous les clichés authentiques de cette journée unique. <br>
+Mon travail est une sorte d’inventaire mémoriel. <br>
+Rendre l’éphémère éternel. <br><br>
+
+Pour voir si ça colle entre nous je peux vous proposer un premier rendez-vous ou même soyons fous, une séance photo de couple, ou mise en scène (ou non) de votre demande, de vos fiançailles..<br><br>
+
+Photographe professionnelle de mariage, <br>
+Diplômée avec plus de vingt ans d’expérience.<br> 
+Belge installée en Suisse, je suis mobile et ouverte à toute proposition.<br>
   </p>
 </article>
