46e6cba6a991b8acde1881ced071dede139ab332
wawatusee <web2020.kieran.labarrere@gmail.com>
2024-10-19 18:03:40 +0200

    main.css, contacts.php, jolie page contact

--- inc/pages/contact.php
+++ inc/pages/contact.php
@@ -1,11 +1,14 @@
 <h2 class="invisible-titre">Contacts</h2>
 <div class="contacts-container">
+    <div class="portrait-contact">
+        <img  src="<?=$repDeco?>portrait-emilie.jpg" alt="Portrait de l'artiste">
+    </div>
     <ul class="contacts">
-        <li> <a href="mailto:info@emilie-foudelman.com">info@emilie-foudelman.com</a>
+        <li> <a class=" contacts-maillink" href="mailto:info@emilie-foudelman.com">info@emilie-foudelman.com</a>
         </li>
-        <li> <a href="tel:+41762416369">Tel : +41 76 241 63 69</a>
+        <li> <a class="contacts-phonelink" href="tel:+41762416369">Tel : +41 76 241 63 69</a>
         </li>
-        <li> <a aria-label="Chat on WhatsApp" href="https://wa.me/+32486100573"><span><img alt="Chat on WhatsApp" src="../public/img/deco//whatsappbtnwithtext.svg"></span></a>
+        <li> <a class="contacts-whatsapplink" aria-label="Chat on WhatsApp" href="https://wa.me/+32486100573">Whatsapp</a>
         </li>
     </ul>
 </div>
