a79ede63a6308d7be091ab4c9cd37582de6363db
wawatusee <web2020.kieran.labarrere@gmail.com>
2024-10-19 14:10:15 +0200

    footer.php, inversion contacts et menu

--- inc/footer.php
+++ inc/footer.php
@@ -1,15 +1,16 @@
 <footer>
     <div class="footerNav">
         <nav class="navfooterbloc">
+            <h3>Menu</h3>
+            <?php echo $menuMain_view ?>
+        </nav>
+        <nav class="navfooterbloc">
             <!--<h3>Contacts</h3>-->
             <a class="maillink" href="mailto:info@emilie-foudelman.com">info@emilie-foudelman.com</a>
             <a class="phonelink" href="tel:+41762416369">Tel : +41 76 241 63 69</a>
             <a class="whatsapplink" aria-label="Chat on WhatsApp" href="https://wa.me/+32486100573">whats app</a>
         </nav>
-        <nav class="navfooterbloc">
-                <h3>Menu</h3>
-            <?php echo $menuMain_view ?>
-        </nav>
+
     </div>
     <nav id="menuRS" class="nav-rs">
         <?php
